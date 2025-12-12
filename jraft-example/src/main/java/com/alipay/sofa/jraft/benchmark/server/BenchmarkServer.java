39edaf96f5c8acf2e478dfb2b9aa25129cd76858
姚庆奇 <yaoqngqi@bilibili.com>
2025-12-12 15:39:45 +0900

    test

--- jraft-example/src/main/java/com/alipay/sofa/jraft/benchmark/server/BenchmarkServer.java
+++ jraft-example/src/main/java/com/alipay/sofa/jraft/benchmark/server/BenchmarkServer.java
@@ -16,6 +16,7 @@
  */
 package com.alipay.sofa.jraft.benchmark.server;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -35,7 +36,7 @@ public class BenchmarkServer {
 
     private static final Logger LOG = LoggerFactory.getLogger(BenchmarkServer.class);
 
-    public static void main(final String[] args) throws InterruptedException {
+    public static void main(final String[] args){
         if (args.length < 3) {
             LOG.error("[configPath] are needed.");
         }
@@ -51,9 +52,9 @@ public class BenchmarkServer {
         Runtime.getRuntime().addShutdownHook(new Thread(node::stop));
         LOG.info("BenchmarkServer start OK, options: {}", opts);
         try {
-            Thread.sleep(Long.MAX_VALUE);
+            new CountDownLatch(1).await();
         } catch (Exception e) {
-            //
+            ///
         }
 
     }
